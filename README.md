0efb5cd508ca6c0a43a7f6b05d0e7ae25c736e44
Cameron Bulock <cameron@bulock.com>
2016-05-05 09:25:29 -0400

    added the ability to set the repeat value by day or month

--- README.md
+++ README.md
@@ -6,3 +6,17 @@ A quickly thrown together tool used to track tasks that need to be done.
 The general idea behind this is, on the /add page, you can add tasks to be done and set a timeframe for how often they need to be completed.
 
 If a task is done, if can be marked completed and will no longer show, until the time frame it should be available to do again.
+
+## Installation
+
+* Clone the repo
+
+* Create a MySQL database
+
+* Run the SQL from /sql/main.sql on the database
+
+* Rename settings.json.rename to settings.json and enter the database details
+
+* Run `composer install`
+
+* Run `bower install`
--- js/add.js
+++ js/add.js
@@ -1,13 +1,30 @@
 $('#add_task').on('submit', function(event){
 	event.preventDefault();
+	var repeat = $('#repeat_days').val();
+	if ($('#repeat_units').val() === 'month') {
+		repeat = Math.round( $('#repeat_months').val() * 30.4 );
+	}
 	TT.call('Task/add', {
 		name: $('#name').val(),
 		desc: $('#desc').val(),
 		priority: $('#priority').val(),
-		repeat: $('#repeat').val()
+		repeat: repeat
 	})
 	.done(function() {
 		$("html, body").animate({ scrollTop: 0 }, "slow");
 		$('#add_task').trigger("reset");
+		$('#month_wrapper').hide();
+		$('#day_wrapper').show().css('display','inline');
 	});
+});
+
+$('#repeat_units').change(function(){
+	if ($(this).val() === 'day') {
+		$('#month_wrapper').hide();
+		$('#day_wrapper').show().css('display','inline');
+	}
+	else {
+		$('#day_wrapper').hide();
+		$('#month_wrapper').show().css('display','inline');
+	}
 });
